3f8ba6cd2ec0346a6872ef21c84fc576642a2c8c
Gilles Pothieu <gillespothieu.pro@outlook.fr>
2019-08-27 20:11:50 +0200

    popover start

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -9,9 +9,21 @@ const AboutMe = () => (
   <div id="aboutMe">
     <div className="aboutMe">
       <h2 className="aboutMe-title">About Me</h2>
-      <p className="aboutMe-littleDescription">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem ab, dignissimos reiciendis fuga porro</p>
-      <p className="aboutMe-mainDescription">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo qui possimus aspernatur laudantium hic saepe quidem maxime nihil sit incidunt unde quam amet nam, quis, similique beatae. Eligendi, eveniet ab? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam ipsam cumque qui doloribus, quo placeat explicabo officiis officia aliquam magnam asperiores illum quae iure ipsum veniam, blanditiis, assumenda omnis repellendus.</p>
-      <h2 className="interests">Interests & Likes</h2>
+      <div className="aboutMe-description">
+        <p className="aboutMe-description-littleDescription">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem ab, dignissimos reiciendis fuga porro</p>
+        <p className="aboutMe-description-mainDescription">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo qui possimus aspernatur laudantium hic saepe quidem maxime nihil sit incidunt unde quam amet nam, quis, similique beatae. Eligendi, eveniet ab? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam ipsam cumque qui doloribus, quo placeat explicabo officiis officia aliquam magnam asperiores illum quae iure ipsum veniam, blanditiis, assumenda omnis repellendus.</p>
+      </div>
+      <div className="aboutMe-interests">
+        <h3 className="aboutMe-interests-title">Interests & Likes</h3>
+        <ul className="aboutMe-interests-ul">
+          <div className="aboutMe-interests-ul-list"> - Sport: <span className="aboutMe-interests-ul-list-italic"> tennis, running, table tennis...</span></div>
+          <div className="aboutMe-interests-ul-list"> - Reading</div>
+          <div className="aboutMe-interests-ul-list"> - Cinema</div>
+          <div className="aboutMe-interests-ul-list"> - Video-Games</div>
+          <div className="aboutMe-interests-ul-list"> - Hanging out with friends</div>
+          <div className="aboutMe-interests-ul-list"> - Video-Games development</div>
+        </ul>
+      </div>
     </div>
   </div>
 );
--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -9,6 +9,17 @@ const Work = () => (
   <div id="work">
     <div className="work">
       <h2 className="work-title">Work</h2>
+      <div className="work-cards">
+        <div className="work-cards-card">
+          <img src="src/materials/images/screen_oboard.jpg" alt="" className="work-cards-card-image" />
+        </div>
+        <div className="work-cards-card">
+          <img src="src/materials/images/screen_oboard.jpg" alt="" className="work-cards-card-image" />
+        </div>
+        <div className="work-cards-card">
+          <img src="src/materials/images/screen_oboard.jpg" alt="" className="work-cards-card-image" />
+        </div>
+      </div>
     </div>
   </div>
 );
