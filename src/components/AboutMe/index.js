b11d5ef22e9a5928d0752f755992225ab0794a18
Gilles Pothieu <gillespothieu.pro@outlook.fr>
2019-09-06 14:01:21 +0200

    test

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -17,7 +17,7 @@ const AboutMe = () => (
         <p className="aboutMe-description-littleDescription" data-aos="fade-left">Hello, I'm Gilles ! I'm a french full stack web developer.</p>
         <p className="aboutMe-description-mainDescription" data-aos="fade-left">
           I just finished &nbsp;
-          <a href="https://oclock.io/"><img src="../../../src/assets/images/svg/logo_oclock.svg" alt="Oclock-icone" className="aboutMe-description-mainDescription-oclock" /></a>
+          <a href="https://oclock.io/"><img src={require(`../../../src/assets/images/svg/logo_oclock.svg`)} alt="logo oclock" className="aboutMe-description-mainDescription-oclock" /></a>
           's school, an amazing remote school. I learned there all I need to start my life as web developer.<br />
           Right now, I'm just keep learning all my brain can accept, waiting for a job opportunity.
           <br />
