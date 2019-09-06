74541f560546a70b30a05fd2100c313ccf5b2792
Gilles Pothieu <gillespothieu.pro@outlook.fr>
2019-09-06 14:04:46 +0200

    test require

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import { Provider } from 'react-redux';
 import App from 'src/components/App';
 
 // Fonts
-import 'src/assets/fonts/Light/tt-firs-light-webfont.woff';
-import 'src/assets/fonts/Hairline/tt-firs-hairline-webfont.woff';
-import 'src/assets/fonts/Bold/tt-firs-bold-webfont.woff';
+require ('src/assets/fonts/Light/tt-firs-light-webfont.woff');
+require ('src/assets/fonts/Hairline/tt-firs-hairline-webfont.woff');
+require ('src/assets/fonts/Bold/tt-firs-bold-webfont.woff');
 
 import store from 'src/store';
 
