e9988d6ee0a5ad582802c3588d70a82ea36df183
Gilles Pothieu <gillespothieu.pro@outlook.fr>
2019-09-08 14:50:52 +0200

    insert tippy test

---
+++ src/components/Contact/PopContent/index.js
@@ -0,0 +1,17 @@
+// == Import : npm
+import React from 'react';
+
+// == Import : local
+import './popContent.scss';
+
+// == Composant
+const PopContent = () => (
+  <div id="popContent">
+    <div className="popContent">
+      Test un deux
+    </div>
+  </div>
+);
+
+// == Export
+export default PopContent;
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,33 +4,48 @@ import React from 'react';
 // == Import : local
 import './contact.scss';
 
+// Import Component
+import PopContent from 'src/components/Contact/PopContent';
+
+// Import Tipy
+import Tippy from '@tippy.js/react';
+
 // == Composant
-const Contact = () => (
-  <div id="contact">
-    <div className="contact">
-      <div className="contact-header">
-        <h2 className="contact-header-title">Contact Me ?</h2>
-      </div>
-      <div className="contact-content">
-        <p className="contact-content-text">If you need informations, or have any question, please, feel free to contact me:</p>
-        <div className="contact-content-number">
-          <p className="contact-content-number-text">My number: <a href="tel:+33619821940"><span className="contact-content-number-text-num">+33 (0)6.19.82.19.40</span></a></p>
+const Contact = () => {
+  
+  const popContent = <PopContent />
+
+  return(
+    <div id="contact">
+      <div className="contact">
+        <div className="contact-header">
+          <h2 className="contact-header-title">Contact Me ?</h2>
         </div>
-        <div className="contact-content-mail">
-          <p className="contact-content-mail-text">My mail: <a href="mailto:gillespothieu.pro@outlook.fr"><span className="contact-content-mail-text-mail">gillespothieu.pro@outlook.fr</span></a></p>
+        <div className="contact-content">
+          <p className="contact-content-text">If you need informations, or have any question, please, feel free to contact me:</p>
+          <div className="contact-content-number">
+            <p className="contact-content-number-text">My number: <a href="tel:+33619821940"><span className="contact-content-number-text-num">+33 (0)6.19.82.19.40</span></a></p>
+          </div>
+          <div className="contact-content-mail">
+            <p className="contact-content-mail-text">My mail: <a href="mailto:gillespothieu.pro@outlook.fr"><span className="contact-content-mail-text-mail">gillespothieu.pro@outlook.fr</span></a></p>
+          </div>
         </div>
-      </div>
-      <div className="contact-cvBox">
-        <div className="contact-cvBox text">
-          Ho, and take a look at my CV !
-        </div>
-        <div className="contact-cvBox-cv">
-          <a href="https://drive.google.com/open?id=1HMcEqJybEKcWfP5nY-U-44sWU0_UdHxE" download>Yo</a>
+        <div className="contact-cvBox">
+          <div className="contact-cvBox text">
+            Ho, and take a look at my CV !
+          </div>
+          <div className="contact-cvBox-cv">
+            <a href="https://drive.google.com/open?id=1HMcEqJybEKcWfP5nY-U-44sWU0_UdHxE" download>Yo</a>
+          </div>
+
+          <Tippy content={popContent} arrow={true} className="test">
+            <button>My button</button>
+          </Tippy>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+}
 
 // == Export
 export default Contact;
