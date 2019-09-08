9de86e3813df9f4feca2e4a94dcba62de6e669c8
Gilles Pothieu <gillespothieu.pro@outlook.fr>
2019-09-08 12:05:43 +0200

    before downloadable content

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -8,7 +8,24 @@ import './contact.scss';
 const Contact = () => (
   <div id="contact">
     <div className="contact">
-      <h2 className="contact-title">contact</h2>
+      <div className="contact-header">
+        <h2 className="contact-header-title">Contact Me ?</h2>
+      </div>
+      <div className="contact-content">
+        <p className="contact-content-text">If you need informations, or have any question, please, feel free to contact me:</p>
+        <div className="contact-content-number">
+          <p className="contact-content-number-text">My number: <a href="tel:+33619821940"><span className="contact-content-number-text-num">+33 (0)6.19.82.19.40</span></a></p>
+        </div>
+        <div className="contact-content-mail">
+          <p className="contact-content-mail-text">My mail: <a href="mailto:gillespothieu.pro@outlook.fr"><span className="contact-content-mail-text-mail">gillespothieu.pro@outlook.fr</span></a></p>
+        </div>
+      </div>
+      <div className="contact-cvBox">
+        <div className="contact-cvBox text">
+          Ho, and take a look at my CV !
+        </div>
+        <div className="contact-cvBox-cv">CV</div>
+      </div>
     </div>
   </div>
 );
