ac730cd36ffd73e68a19891c20938f45e57f5ac9
Gilles Pothieu <gillespothieu.pro@outlook.fr>
2019-09-09 16:40:03 +0200

    code amelioration

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -7,6 +7,10 @@ import './work.scss';
 // AnchorLink Scroll
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 
+// BackgroundImages
+import PortfolioImage from 'src/assets/images/jpg/portfolio.jpg';
+import WorkInProgressImage from 'src/assets/images/png/work_in_progress.png';
+
 // Import logos:
 import {
   FaGithub,
@@ -19,7 +23,7 @@ const Work = () => (
       <div className="work-wrapper">
         <h2 className="work-wrapper-title" data-aos="fade-right">Work</h2>
         <div className="work-wrapper-cards" data-aos="fade">
-          <div className="work-wrapper-cards-card">
+          <div className="work-wrapper-cards-card" style={{ backgroundImage: `url(${PortfolioImage})` }}>
             <div className="work-wrapper-cards-card-overlay">
               <div className="work-wrapper-cards-card-overlay-titleContent">
                 <p className="work-wrapper-cards-card-overlay-titleContent-title">Gilles' Portfolio</p>
@@ -35,73 +39,29 @@ const Work = () => (
               </div>
               <div className="work-wrapper-cards-card-overlay-footer">
                 <div className="work-wrapper-cards-card-overlay-footer-date">Septembre 2019</div>
-                <a href="https://github.com/PothieuG/Portfolio" className="work-wrapper-cards-card-overlay-footer-github">
+                <a href="#" className="work-wrapper-cards-card-overlay-footer-github">
                   <FaGithub className="work-wrapper-cards-card-overlay-footer-github-icon " width="100%" />
                 </a>
               </div>
             </div>
           </div>
-          <div className="work-wrapper-cards-card">
+          <div className="work-wrapper-cards-card" style={{ backgroundImage: `url(${WorkInProgressImage})` }}>
             <div className="work-wrapper-cards-card-overlay">
               <div className="work-wrapper-cards-card-overlay-titleContent">
-                <p className="work-wrapper-cards-card-overlay-titleContent-title">Gilles' Portfolio</p>
+                <p className="work-wrapper-cards-card-overlay-titleContent-title">Work In Progress</p>
               </div>
               <hr />
               <div className="work-wrapper-cards-card-overlay-infos">
                 <div className="work-wrapper-cards-card-overlay-infos-text">
-                  Just where you are :)
+                  Comming soon !
                 </div>
               </div>
               <div className="work-wrapper-cards-card-overlay-visit">
-                <AnchorLink href="#app"><p className="work-wrapper-cards-card-overlay-visit-text">Visit</p></AnchorLink>
+                <p className="work-wrapper-cards-card-overlay-visit-text">Visit</p>
               </div>
               <div className="work-wrapper-cards-card-overlay-footer">
-                <div className="work-wrapper-cards-card-overlay-footer-date">Septembre 2019</div>
-                <a href="https://github.com/PothieuG/Portfolio" className="work-wrapper-cards-card-overlay-footer-github">
-                  <FaGithub className="work-wrapper-cards-card-overlay-footer-github-icon " width="100%" />
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="work-wrapper-cards-card">
-            <div className="work-wrapper-cards-card-overlay">
-              <div className="work-wrapper-cards-card-overlay-titleContent">
-                <p className="work-wrapper-cards-card-overlay-titleContent-title">Gilles' Portfolio</p>
-              </div>
-              <hr />
-              <div className="work-wrapper-cards-card-overlay-infos">
-                <div className="work-wrapper-cards-card-overlay-infos-text">
-                  Just where you are :)
-                </div>
-              </div>
-              <div className="work-wrapper-cards-card-overlay-visit">
-                <AnchorLink href="#app"><p className="work-wrapper-cards-card-overlay-visit-text">Visit</p></AnchorLink>
-              </div>
-              <div className="work-wrapper-cards-card-overlay-footer">
-                <div className="work-wrapper-cards-card-overlay-footer-date">Septembre 2019</div>
-                <a href="https://github.com/PothieuG/Portfolio" className="work-wrapper-cards-card-overlay-footer-github">
-                  <FaGithub className="work-wrapper-cards-card-overlay-footer-github-icon " width="100%" />
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="work-wrapper-cards-card">
-            <div className="work-wrapper-cards-card-overlay">
-              <div className="work-wrapper-cards-card-overlay-titleContent">
-                <p className="work-wrapper-cards-card-overlay-titleContent-title">Gilles' Portfolio</p>
-              </div>
-              <hr />
-              <div className="work-wrapper-cards-card-overlay-infos">
-                <div className="work-wrapper-cards-card-overlay-infos-text">
-                  Just where you are :)
-                </div>
-              </div>
-              <div className="work-wrapper-cards-card-overlay-visit">
-                <AnchorLink href="#app"><p className="work-wrapper-cards-card-overlay-visit-text">Visit</p></AnchorLink>
-              </div>
-              <div className="work-wrapper-cards-card-overlay-footer">
-                <div className="work-wrapper-cards-card-overlay-footer-date">Septembre 2019</div>
-                <a href="https://github.com/PothieuG/Portfolio" className="work-wrapper-cards-card-overlay-footer-github">
+                <div className="work-wrapper-cards-card-overlay-footer-date">Soon</div>
+                <a href="#" className="work-wrapper-cards-card-overlay-footer-github">
                   <FaGithub className="work-wrapper-cards-card-overlay-footer-github-icon " width="100%" />
                 </a>
               </div>
